1824c9b40ad23c71c9e957be39025ceca1824ab7
Vivian G <vivian.grimaux@hotmail.fr>
2019-09-11 12:52:51 +0200

    Rajout d'une page avec un lien vers la boucle

---
+++ index2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    for ($i=0; $i < 10; $i++) { 
+        ?><p><?php echo "Viens ici mon frerrrrr -----> "."<a href='11_09_2019.php'>Lien de folie</a>";?> </p><?php
+    }
+    ?>
+</body>
+</html>
